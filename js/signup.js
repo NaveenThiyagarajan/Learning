5d2e8c73b40726ce6a124df3557a4176402fccdc
NAVEEN THIYAGARAJAN <naveen11.gct@gmail.com>
2018-11-25 13:12:27 +0530

    signup suffs

---
+++ js/signup.js
@@ -0,0 +1,20 @@
+function signup() {
+	const fName = document.querySelector('input[name="FirstName"]').value;
+	const lName = document.querySelector('input[name="LastName"]').value;
+	const emailID = document.querySelector('input[name="EmailID"]').value;
+	const password = document.querySelector('input[name="Password"]').value;
+	const confirmPassword = document.querySelector('input[name="ConfirmPassword"]').value;
+	const emailRegex = RegExp(/^[a-zA-Z0-9.]{3,}@[a-zA-Z0-9]{2,}.[a-zA-Z]{3,}$/);
+	let text = '<strong style="color: green;">Success!!</strong>'
+
+	if(password !== confirmPassword){
+		text =  '<strong style="color: red;">Make sure both password matches!!</strong>'
+	}
+	if(!emailRegex.test(emailID)){
+		text = "<strong style=\"color: red;\">emailID is not proper!!!</strong>";
+	}
+	if(!fName || !lName){
+		text =  '<strong style="color: red;">Make sure enter both firstName and lastName!!</strong>'
+	}
+	document.getElementById("status").innerHTML = text;
+}
